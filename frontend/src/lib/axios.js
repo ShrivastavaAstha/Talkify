28eee0e43bc96068a64f0f07a90f8cd443be52ee
ShrivastavaAstha <astha2333@gmail.com>
2025-09-05 14:44:01 +0530

    first commit

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_URL;
+// const BASE_URL = import.meta.env.VITE_API_URL;
 
-export const axiosInstance = axios.create({
-  baseURL: BASE_URL,
-  withCredentials: true,
-});
+// export const axiosInstance = axios.create({
+//   baseURL: BASE_URL,
+//   withCredentials: true,
+// });
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api"
+    : "https://talkify-znj9.onrender.com";
